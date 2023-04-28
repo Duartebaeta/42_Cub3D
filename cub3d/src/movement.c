e2b6bb9fbd69077c6bb3fbe0e326db8aac3d83fa
JonasBuchadas <jonasmcbuchadas@gmail.com>
2023-04-28 23:07:46 +0100

    Check wall collisions

--- cub3d/src/movement.c
+++ cub3d/src/movement.c
@@ -3,22 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   movement.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dhomem-d <dhomem-d@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jocaetan <jocaetan@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 17:01:02 by dhomem-d          #+#    #+#             */
-/*   Updated: 2023/04/26 10:54:04 by dhomem-d         ###   ########.fr       */
+/*   Updated: 2023/04/28 23:05:15 by jocaetan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/cub3d.h"
 
+bool check_collisions(double move_x, double move_y);
+
 void move(double relative_angle)
 {
 	double final_angle = cub()->player.angle + relative_angle;
-	
+
 	double move_x = 0.25 * cos(final_angle);
 	double move_y = 0.25 * sin(final_angle);
-
+	if (check_collisions(move_x, move_y))
+		return ;
 	cub()->player.x += move_x;
 	cub()->player.y += move_y;
+}
+
+bool check_collisions(double move_x, double move_y) {
+	int map_x;
+	int map_y;
+
+	map_x = (int)floor(cub()->player.x + move_x);
+	map_y = (int)floor(cub()->player.y + move_y);
+
+	if (cub()->map[map_y][map_x] == '1')
+		return (true);
+	return (false);
 }
